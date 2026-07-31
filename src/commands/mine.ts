d3911f378863b39edf9a53c72c8ea9de5310d293
Gabriel Marques <gabsfrmarqs>
2026-07-31 18:58:56 -0300

    Added more server info to the embed

--- src/commands/mine.ts
+++ src/commands/mine.ts
@@ -5,33 +5,30 @@ import { EmbedBuilder } from "discord.js";
 import { writeFileSync } from "fs";
 import { MinecraftRconService } from "./minecraft-rcon.js";
 
-async function getServerStatus(url: string){
-    const requestOptions: RequestInit = {
-        method: "GET",
-        redirect: "follow" as RequestRedirect
-    };
-    let status;
-    try {
+async function getServerStatus(url: string) {
+  const requestOptions: RequestInit = {
+    method: "GET",
+    redirect: "follow" as RequestRedirect,
+  };
+  let status;
+  try {
     let fetchResponse = await fetch(url, requestOptions);
     status = await fetchResponse.json();
-    
-  }
-  catch (error) {
+  } catch (error) {
     console.log("fudeu");
-    status = {error: "error"}
-    
+    status = { error: "error" };
   }
   console.log(status);
   return status;
 }
 
-function base64toPng(base64String: string){
-    const base64Data = base64String.replace(/^data:image\/png;base64,/, "");
-    const buffer = Buffer.from(base64Data, "base64");
-    const filePath = "./server-icon.png";
-    writeFileSync(filePath, buffer);
-    console.log(`Image saved to ${filePath}`);
-    return filePath;
+function base64toPng(base64String: string) {
+  const base64Data = base64String.replace(/^data:image\/png;base64,/, "");
+  const buffer = Buffer.from(base64Data, "base64");
+  const filePath = "./server-icon.png";
+  writeFileSync(filePath, buffer);
+  console.log(`Image saved to ${filePath}`);
+  return filePath;
 }
 
 @Discord()
@@ -39,16 +36,18 @@ function base64toPng(base64String: string){
 export class GroupExample {
   @Slash({ description: "status" })
   @SlashGroup("mine")
-  async status(
-    interaction: CommandInteraction,
-  ): Promise<void> {
+  async status(interaction: CommandInteraction): Promise<void> {
     // Defer the reply immediately to prevent timeout
     await interaction.deferReply();
 
     // Java Server data
-    let javaData = await getServerStatus("https://api.mcsrvstat.us/3/minecraft.meltylands.boo");
-    let bedrockData = await getServerStatus("https://api.mcsrvstat.us/bedrock/3/bedrock.marquesgabriel.dev:25565");
-    
+    let javaData = await getServerStatus(
+      "https://api.mcsrvstat.us/3/minecraft.meltylands.boo",
+    );
+    let bedrockData = await getServerStatus(
+      "https://api.mcsrvstat.us/bedrock/3/bedrock.meltylands.boo.dev:25565",
+    );
+
     // Check if Java data is valid
     if (!javaData || javaData.error) {
       await interaction.editReply("Erro ao obter status do servidor Java.");
@@ -58,51 +57,68 @@ export class GroupExample {
     const unixtime = Math.floor(Date.now() / 1000);
     let ultimaChecagem = Math.floor(unixtime - javaData.debug.cachetime);
 
-    if (javaData.online !== true){
+    if (javaData.online !== true) {
       await interaction.editReply(`Servidor offline! Ultima verificação ocorreu ${ultimaChecagem} segundos atrás.
           \nAguarde ${300 - ultimaChecagem} segundos para tentar novamente.`);
       return;
     }
 
     let playersList = [];
-    let playersString = `${javaData.players.online}/${javaData.players.max}`
+    let playersString = `${javaData.players.online}/${javaData.players.max}`;
     if (javaData.players.list) {
       for (const i in javaData.players.list) {
         playersString += `\n${javaData.players.list[i].name}`;
       }
     }
+
+    const iconPath = javaData.icon
+      ? base64toPng(javaData.icon)
+      : "./server-icon.png";
+    const file = new AttachmentBuilder(iconPath, { name: "server-icon.png" });
+    const attachment = new AttachmentBuilder(iconPath, {
+      name: "server-icon.png",
+    });
+    const embed = new EmbedBuilder();
+    embed.setFooter({
+      text: `Ultima verificação ocorreu ${ultimaChecagem} segundos atrás`,
+    });
+    embed.setTitle("**Server Online**");
+    embed.addFields({ name: "Hostname", value: javaData.hostname });
+    if (!bedrockData.error && bedrockData.online === true) {
+      embed.addFields({
+        name: "Bedrock Server",
+        value: `bedrock.meltylands.boo`,
+      });
+    }
+
+    //motd
+    embed.addFields({
+      name: "Description",
+      value: javaData.motd.clean.join("\n") || "-",
+    });
+
+    //Online Players list
+    embed.addFields({ name: "Players Online", value: playersString });
+
+    //Minecraft version
+
+    if ((javaData.software).search(/Paper/) != -1){
+      embed.addFields({ name: "Version", value: javaData.version + " com plugins"});
+    } else{
+        embed.addFields({ name: "Version", value: javaData.version});
+    }
     
-    const iconPath = javaData.icon ? base64toPng(javaData.icon) : "./server-icon.png";
-  const file = new AttachmentBuilder(iconPath, { name: 'server-icon.png' });
-  const attachment = new AttachmentBuilder(iconPath, { name: 'server-icon.png' });
-  const embed = new EmbedBuilder();
-  embed.setFooter({
-    text: `Ultima verificação ocorreu ${ultimaChecagem} segundos atrás`,
-  })
-  embed.setTitle("**Server Online**")
-  embed.addFields({ name: "Hostname", value: javaData.hostname })
-  if (!bedrockData.error && bedrockData.online === true) {
-    embed.addFields({ name: "Bedrock Server", value: `bedrock.meltylands.boo` })
-  }
-  embed.addFields({
-    name: "Description",
-    value: javaData.motd.clean.join("\n") || "-",
-  })
-  embed.addFields({ name: "Players Online", value: playersString })
-  embed.setThumbnail('attachment://server-icon.png')
+
+    embed.setThumbnail("attachment://server-icon.png");
     await interaction.editReply({ embeds: [embed], files: [file] });
   }
 
   @Slash({ description: "mapa" })
   @SlashGroup("mine")
-  async mapa(
-    interaction: CommandInteraction,
-  ): Promise<void> {
+  async mapa(interaction: CommandInteraction): Promise<void> {
     await interaction.deferReply();
-    await interaction.editReply("Aqui está o link do mapa! \nhttps://bluemap.meltylands.boo");
+    await interaction.editReply(
+      "Aqui está o link do mapa! \nhttps://bluemap.meltylands.boo",
+    );
   }
-
 }
-
-
-
